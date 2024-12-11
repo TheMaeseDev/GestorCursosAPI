eeb86432b4fb0dcf72cfd8f6a524ddee9c03b8b2
TheMaeseDev <diegoalvarez.devpr@gmail.com>
2024-12-11 16:25:30 -0300

    Dtos de Estudiantes, y Estudiante controller actualizado.

---
+++ GestorCursosAPI/DTOs/Estudiantes/EstudianteCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GestorCursosAPI.DTOs.Estudiantes
+{
+    public class EstudianteCreateDto
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ GestorCursosAPI/DTOs/Estudiantes/EstudianteReadDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GestorCursosAPI.DTOs.Estudiantes
+{
+    public class EstudianteReadDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public List<int> CursosIds { get; set; } = new List<int>(); // Lista de IDs de cursos relacionados
+    }
+}
