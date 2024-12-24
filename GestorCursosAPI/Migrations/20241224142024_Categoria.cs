d3f31241054a091fc4f8c00a744b041e940cbd63
TheMaeseDev <diegoalvarez.devpr@gmail.com>
2024-12-24 11:27:37 -0300

    Categoria: Service, repository

---
+++ GestorCursosAPI/Migrations/20241224142024_Categoria.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestorCursosAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Categoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cursos_Categoria_CategoriaId",
+                table: "Cursos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categoria",
+                table: "Categoria");
+
+            migrationBuilder.RenameTable(
+                name: "Categoria",
+                newName: "Categorias");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cursos_Categorias_CategoriaId",
+                table: "Cursos",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cursos_Categorias_CategoriaId",
+                table: "Cursos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias");
+
+            migrationBuilder.RenameTable(
+                name: "Categorias",
+                newName: "Categoria");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categoria",
+                table: "Categoria",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cursos_Categoria_CategoriaId",
+                table: "Cursos",
+                column: "CategoriaId",
+                principalTable: "Categoria",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ GestorCursosAPI/Services/CategoriaServices/ICategoriaService.cs
@@ -0,0 +1,13 @@
+﻿using GestorCursosAPI.Models;
+
+namespace GestorCursosAPI.Services.CategoriaServices
+{
+    public interface ICategoriaService
+    {
+        Task<List<Categoria>> GetAllAsync();
+        Task<Categoria> GetByIdAsync(int id);
+        Task AddAsync(Categoria categoria);
+        Task UpdateAsync(Categoria categoria);
+        Task DeleteAsync(int id);
+    }
+}
